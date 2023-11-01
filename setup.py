3ede923c2b88db38e999f584365432505694d098
Juan Alvarez <jfhack7@gmail.com>
2023-11-01 04:20:10 -0300

    v1.1, pypi.org project description

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+  long_description = fh.read()
+
 setup(
-    name="pdf-unlocker",
-    version="1.0",
-    packages=find_packages(),
-    scripts=['scripts/pdf-unlocker'],
-    install_requires=[
-        'pikepdf'
-    ],
+  name="pdf-unlocker",
+  version="1.1",
+  packages=find_packages(),
+  scripts=['scripts/pdf-unlocker'],
+  install_requires=[
+    'pikepdf'
+  ],
+  long_description=long_description,
+  long_description_content_type="text/markdown"
 )
